e636a957ddde4cf0d9e7851c6e0f5fcb18c35d41
ahmednegm93 <ahmednegm.developper93@gmail.com>
2023-10-09 00:37:54 +0300

    add job app model

--- job/models.py
+++ job/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+job_types=((
+'full time','full time'
+),(
+    'part time','part time'
+)
+           )
+
+
 # Create your models here.
+class job(models.Model):
+    title=models.CharField(max_length=100)
+    job_type=models.CharField(choices=job_types,max_length=50)
+    describtion=models.TextField(max_length=100)
+    published_at=models.DateTimeField(auto_now=True)
+    vacancy=models.IntegerField(default=1)
+    salary=models.IntegerField(default=0)
+    experience=models.IntegerField(default=1)
+   
--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'job',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_job_board',
+        'USER':  'postgres',
+        'PASSWORD': '122122',
+        'Host' : 'localhost',
+        'PORT': '5432',
     }
 }
 
